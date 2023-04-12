2cd35824be45f2a240ca2ec5493c3109a2f8cef5
Ömer Güven <omr.gven@gmail.com>
2023-04-12 23:40:27 +0300

    update list

--- lib/infinite_list_delegate.dart
+++ lib/infinite_list_delegate.dart
@@ -1,81 +1,78 @@
 library infinite_list_delegate;
 
 import 'dart:async';
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
-class InfiniteListDelegate extends SliverChildBuilderDelegate {
-  InfiniteListDelegate({
-    required this.itemBuilder,
+class InfiniteChildBuilderDelegate extends SliverChildBuilderDelegate {
+  static Widget _defaultSeparatedBuilder(BuildContext context, int index) {
+    return const SizedBox.shrink();
+  }
+
+  InfiniteChildBuilderDelegate({
     required this.itemCount,
+    required this.itemBuilder,
     required this.onInfinite,
-    this.separatorBuilder,
-    this.progressIndicator,
+    this.canInfinite = true,
+    this.separatedBuilder = _defaultSeparatedBuilder,
     this.endOfListWidget = const SizedBox.shrink(),
+    this.progressIndicator = const Padding(
+      padding: EdgeInsets.symmetric(vertical: 40),
+      child: Center(child: CircularProgressIndicator.adaptive()),
+    ),
   }) : super(
           (context, index) {},
-          childCount: max(0, itemCount * 2) + 1,
-          semanticIndexCallback: (Widget _, int index) {
+          childCount: math.max(0, itemCount * 2) + 1,
+          semanticIndexCallback: (_, index) {
             return index.isEven ? index ~/ 2 : null;
           },
+          addRepaintBoundaries: true,
+          addAutomaticKeepAlives: true,
+          addSemanticIndexes: true,
         );
 
-  final FutureOr<bool> Function() onInfinite;
-  final Widget Function(BuildContext, int) itemBuilder;
-  final Widget Function(BuildContext, int)? separatorBuilder;
+  final Future<void> Function() onInfinite;
+  final Widget Function(BuildContext context, int index) itemBuilder;
+  final Widget Function(BuildContext context, int index) separatedBuilder;
   final int itemCount;
 
-  final Widget? progressIndicator;
+  final Widget progressIndicator;
   final Widget endOfListWidget;
+  final bool canInfinite;
 
-  var canInfinite = ValueNotifier(true);
   var isWorking = false;
 
   Future<void> onInfiniteHandler() async {
-    if (isWorking || !canInfinite.value || itemCount == 0) {
+    if (isWorking || !canInfinite || itemCount == 0) {
       return;
     }
 
     isWorking = true;
-    canInfinite.value = await onInfinite();
+    await onInfinite();
 
-    if (canInfinite.value) {
+    if (canInfinite) {
       isWorking = false;
     }
   }
 
   @override
-  NullableIndexedWidgetBuilder get builder => (context, index) {
-        final itemIndex = index ~/ 2;
+  NullableIndexedWidgetBuilder get builder => (context, i) {
+        final index = i ~/ 2;
 
-        if (itemIndex >= itemCount) {
-          return ValueListenableBuilder(
-            valueListenable: canInfinite,
-            builder: (context, iCan, indicator) {
-              if (iCan) {
-                onInfiniteHandler();
-                return indicator!;
-              }
+        if (index >= itemCount) {
+          if (canInfinite) {
+            onInfiniteHandler();
+            return progressIndicator;
+          }
 
-              return endOfListWidget;
-            },
-            child: progressIndicator ??
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 40),
-                  child: Center(child: CircularProgressIndicator.adaptive()),
-                ),
-          );
+          return endOfListWidget;
         }
 
-        if (index.isEven) {
-          return itemBuilder(context, itemIndex);
+        if (i.isEven) {
+          return itemBuilder(context, index);
         }
 
-        if (separatorBuilder != null) {
-          return separatorBuilder!(context, itemIndex);
-        }
-
-        return const SizedBox.shrink();
+        return separatedBuilder(context, index);
       };
 }
